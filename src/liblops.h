a8c4883e20d75483bce610b193ac4f0cba849277
Mikhail Gusarov <dottedmag@dottedmag.net>
2009-09-03 03:20:24 +0700

    Rename lread->readn, lwrite->writen to be compatible with APUE

--- src/liblops.h
+++ src/liblops.h
@@ -7,12 +7,18 @@
  * This function is like read(2), but it reads until eof, requested size is read
  * or error occured.
  */
-ssize_t lread(int fd, void* buf, size_t count);
+ssize_t readn(int fd, void* buf, size_t count);
 
 /*
  * This function is like write(2), but it writes until everything is written or
  * error occured.
  */
+ssize_t writen(int fd, const void* buf, size_t count);
+
+/*
+ * Compatibility wrappers
+ */
+ssize_t lread(int fd, void* buf, size_t count);
 ssize_t lwrite(int fd, const void* buf, size_t count);
 
 #endif
